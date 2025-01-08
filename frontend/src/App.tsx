4baead45504d0dbaf91aff79ac2ed4d0b9ab3443
GDMontsho <montshogoitseone@gmail.com>
2025-01-08 10:28:47 +0100

    intergrating M + G App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { Box, Button, Typography } from "@mui/material";
 import { ThemeContextProvider } from "./state/ThemeContext";
 import Navbar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
     <ThemeContextProvider>
       <Navbar />
+      <HeroSection />
     </ThemeContextProvider>
   );
 };
