20707ac2a15c7090be0923761b459b9ec9cd2988
GDMontsho <montshogoitseone@gmail.com>
2025-01-08 13:56:07 +0100

    register/login routes Fardin Goitsy

---
+++ backend/routes/auth.js
@@ -0,0 +1,57 @@
+import express from "express";
+import userModel from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+// Register User
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await userModel.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new userModel({ name, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+});
+
+// Login User
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connect } from "./config/db.js";
-// import userModel from "./models/UserModel.js"
+import authRoutes from "./routes/auth.js";
 
 // App Config
 dotenv.config();
@@ -10,17 +10,12 @@ connect();
 const app = express();
 const PORT = process.env.PORT || 6000;
 
-
-
-
-
-
 // middlewares
 app.use(express.json());
 app.use(cors());
 
 //api endpoints
-
+app.use("/api/auth", authRoutes);
 // app.get("/", (req, res) => {
 //   res.send("API Woriking");
 // });
