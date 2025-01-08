16183ef34ce6bdf0dcc86f1ce570eaaf804ffc27
Fardinazimi <fardin.azimi@dci-student.org>
2025-01-08 14:18:00 +0100

    server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connect } from "./config/db.js";
-// import userModel from "./models/UserModel.js"
+import authRoutes from "./routes/auth.js"
 
 // App Config
 dotenv.config();
