256f362bbf3e6a439ce3bfbb08e5560c71fb9473
Fardinazimi <fardin.azimi@dci-student.org>
2025-01-20 13:49:26 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connect } from "./config/db.js";
+import authRoutes from "./routes/auth.js"
 import authRoutes from "./routes/auth.js";
 import jobAppRoutes from "./routes/jobAppRoutes.js";
 
